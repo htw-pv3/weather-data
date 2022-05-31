edddb731e0667b16a83304b3a8fa9812c6a72a5c
Ludee <Ludwig.Huelk@rl-institut.de>
2022-05-31 22:04:58 +0200

    Update and format main.py #38

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@
 """
 HTW-PV3 - Weather Data - Main file
 
-Export weatherdata for PVSOL and POLYSUN
+Export weather data for PVSOL and Polysun
 
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
@@ -18,27 +18,17 @@ __version__ = "v0.0.1"
 from settings import postgres_session
 from pv3_export_polysun import export_htw_polysun
 
-import sys
-import getpass
 import pandas as pd
 from sqlalchemy import *
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-import plotly.graph_objs as go
-import plotly.offline as pltly
-import colorlover as cl
-import seaborn as sns
 
 version = 'v1 (jupyter)'
 project = 'pv3'
 
-
 if __name__ == "__main__":
-
-    # start session
+    # Start database session
     con = postgres_session()
 
-    # Database select (SQL)
+    # Select data
     sql = text("""
             SELECT  *                                  -- column
             FROM    pv3.pv3_weather_2015_filled_mview  -- table
@@ -46,12 +36,11 @@ if __name__ == "__main__":
     df_htw = pd.read_sql_query(sql, con)
     df_htw = df_htw.set_index('timestamp')
 
-
-    """Export data"""
-    # Export HTW for POLYSUN
+    # Export data
     fn_polysun = 'pv3_htw_polysun_1min_2015.csv'
     export_htw_polysun(df_htw, fn_polysun, 'M', 'g_hor_si')
 
     file_name_polysun_1h = 'pv3_htw_polysun_1h_2015.csv'
     df_db_htw_weather_1h = df_htw.resample('1H').mean()
-    export_htw_polysun(df_db_htw_weather_1h, file_name_polysun_1h, 'H', 'g_hor_si')
+    export_htw_polysun(df_db_htw_weather_1h, file_name_polysun_1h, 'H',
+                       'g_hor_si')
