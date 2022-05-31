4fe7f7f7aaed87b1553b41869e138bcd501e50ba
Ludee <Ludwig.Huelk@rl-institut.de>
2022-05-31 22:00:09 +0200

    Update and format export polysun #38

--- python/pv3_export_polysun.py
+++ python/pv3_export_polysun.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-HTW-PV3 - Export weather data for POLYSUN
+HTW-PV3 - Export weather data for Polysun
 
-Export weatherdata for POLYSUN
+Export weather data for Polysun
 
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
@@ -15,19 +15,38 @@ __url__ = "https://www.gnu.org/licenses/agpl-3.0.en.html"
 __author__ = "Ludee;"
 __version__ = "v0.0.1"
 
-import os
-import pandas as pd
-
 from irradiation import calculate_diffuse_irradiation
 from settings import write_to_csv
 from settings import HTW_LAT, HTW_LON
 
+import pandas as pd
+
+
 def export_htw_polysun(df_weather, filename, resolution, parameter_name):
-    """TODO: Docu"""
+    """
+    Export weather data for Polysun
 
-    # calc diffuse irradiation
-    df_dhi = calculate_diffuse_irradiation(df_weather, parameter_name, HTW_LAT, HTW_LON)
+    Parameters
+    ----------
+    df_weather : DataFrame
+        Weather data
+    filename : str
+        Name of csv file
+    resolution : str
+        Resolution for time series: Minute (M), Hour (H)
+    parameter_name :
+        Name of column with GHI for diffuse irradiation
+    Returns
+    -------
+    file : csv
+        Calculated weather data in Polysun format
+    """
 
+    # Calculate diffuse irradiation
+    df_dhi = calculate_diffuse_irradiation(df_weather, parameter_name, HTW_LAT,
+                                           HTW_LON)
+
+    # Set resolution for time index in seconds
     if resolution == 'M':
         s = 60
         steps = 525600
@@ -48,17 +67,15 @@ def export_htw_polysun(df_weather, filename, resolution, parameter_name):
     df_polysun = df_polysun.merge(df_dhi['dhi'].reset_index(drop=True),
                                   left_index=True, right_index=True)
 
-    # zero values as not needed
-    df_polysun['Lh [W/m²]'] = 0                 # Lh Langwellenstrahlung[Wh / m2]
+    df_polysun['Lh [W/m²]'] = 0  # Lh Langwellenstrahlung[Wh / m2]
 
-    # rename columns
-    PRINT_NAMES = {'g_hor_si': 'Gh [W/m²]',     # Gh Globalstrahlung [Wh/m2]
-                   'dhi': 'Dh [W/m²]',          # Dh Diffusstrahlung [Wh/m2]
-                   't_luft': 'Tamb [°C]',       # Tamb Umgebungstemperatur [°C]
-                   'v_wind': 'Wind [m/s]',      # Vwnd Windgeschwindigkeit [m/s]
-                   'h_luft': 'Humidity [%]',    # Hrel Luftfeuchtigkeit [%]
-                   }
-    df_polysun = df_polysun.rename(columns=PRINT_NAMES)
+    column_names = {'g_hor_si': 'Gh [W/m²]',  # Gh Globalstrahlung [Wh/m2]
+                    'dhi': 'Dh [W/m²]',  # Dh Diffusstrahlung [Wh/m2]
+                    't_luft': 'Tamb [°C]',  # Tamb Umgebungstemperatur [°C]
+                    'v_wind': 'Wind [m/s]',  # Vwnd Windgeschwindigkeit [m/s]
+                    'h_luft': 'Humidity [%]',  # Hrel Luftfeuchtigkeit [%]
+                    }
+    df_polysun = df_polysun.rename(columns=column_names)
 
     df_polysun = df_polysun.loc[:,
                  ['# Time [s]', 'Gh [W/m²]', 'Dh [W/m²]', 'Tamb [°C]',
@@ -69,19 +86,14 @@ def export_htw_polysun(df_weather, filename, resolution, parameter_name):
     write_to_csv(f'./data/{filename}', df_polysun, append=False,
                  index=False)
 
-    ## 1. Todo Doku
+    # Add header rows
     polysun_first_row = '# Station: HTW Berlin, PVlib\n'
-    ## 2. Todo Doku
     polysun_second_row = f'# Latitude: {HTW_LAT:.4f} Longitude: {HTW_LON:.4f} altitude: 81m\n'
-    ## 3. Todo Doku
     polysun_third_row = '#'
+
+    # Write to file
     with open(f'./data/{filename}', "r+") as text_file:
         content = text_file.read()
         text_file.seek(0, 0)
         text_file.write(
             polysun_first_row + polysun_second_row + polysun_third_row + '\n' + content)
-
-
-
-
-
