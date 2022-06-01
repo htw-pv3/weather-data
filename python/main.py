de9bbea960738cee5ff4d9c6454078fc41d0ffbf
Ludee <Ludwig.Huelk@rl-institut.de>
2022-06-01 12:12:22 +0200

    Add plot for minute resolution #45

--- python/main.py
+++ python/main.py
@@ -36,14 +36,39 @@ if __name__ == "__main__":
 
     # Export data
     fn_polysun = 'pv3_htw_polysun_1min_2015.csv'
-    df_irradiance = export_htw_polysun(df_htw, fn_polysun, 'M', 'g_hor_si')
+    df_irradiance_min = export_htw_polysun(df_htw, fn_polysun, 'M', 'g_hor_si')
 
     file_name_polysun_1h = 'pv3_htw_polysun_1h_2015.csv'
-    df_db_htw_weather_1h = df_htw.resample('1H').mean()
-    export_htw_polysun(df_db_htw_weather_1h, file_name_polysun_1h, 'H',
-                       'g_hor_si')
+    df_htw_hour = df_htw.resample('1H').mean()
+    df_irradiance_hour = export_htw_polysun(df_htw_hour, file_name_polysun_1h,
+                                            'H',
+                                            'g_hor_si')
 
-    # Plot
+    # Plot Minute
+    info_dict = {}
+    info_dict['Filename'] = ['{}_htw_weatherdata'.format(
+        pd.to_datetime('today').strftime("%Y-%m-%d"))]
+    info_dict['Value'] = ['Bestrahlungsstärke']
+    info_dict['Unit'] = ['W/m²']
+    info_dict['Y-Axis'] = [
+        '{} in {}'.format(*info_dict['Value'], *info_dict['Unit'])]
+    info_dict['X-Axis'] = ['Minute im Jahr']
+    info_dict['Title'] = ['Solarstrahlung']
+    meta_str = query_database_metadata(con, schema, table)
+    info_dict['Metadata'] = [meta_str]
+
+    sns.set_palette("husl")
+    mfig_weatherdata_min = plt.figure(figsize=(12, 5))
+    ax = plt.subplot()
+    plt.title(*info_dict['Title'], fontsize=16)
+    plt.ylabel(*info_dict['Y-Axis'])
+    plt.xlabel(*info_dict['X-Axis'])
+    df_irradiance_min.loc[:, ['dni', 'dhi']].plot(ax=ax,
+                                                  lw=0.5,
+                                                  markeredgewidth=3)
+    ax.set_ylim(ymin=0)
+
+    # Plot Hour
     info_dict = {}
     info_dict['Filename'] = ['{}_htw_weatherdata'.format(
         pd.to_datetime('today').strftime("%Y-%m-%d"))]
@@ -57,14 +82,21 @@ if __name__ == "__main__":
     info_dict['Metadata'] = [meta_str]
 
     sns.set_palette("husl")
-    mfig_weatherdata = plt.figure(figsize=(12, 5))
+    mfig_weatherdata_hour = plt.figure(figsize=(12, 5))
     ax = plt.subplot()
-    plt.title(*info_dict['Title'], fontsize=16)  # Title from info
-    plt.ylabel(*info_dict['Y-Axis'])  # Label from info
-    df_irradiance.resample('1H').mean().plot(ax=ax, lw=0.5, markeredgewidth=3)
+    plt.title(*info_dict['Title'], fontsize=16)
+    plt.ylabel(*info_dict['Y-Axis'])
+    plt.xlabel(*info_dict['X-Axis'])
+    df_irradiance_min.loc[:, ['dni', 'dhi']].resample('1H').mean().plot(ax=ax,
+                                                                        lw=0.5,
+                                                                        markeredgewidth=3)
     ax.set_ylim(ymin=0)
 
-    # Save plot as file
-    plotname = 'data/{}_mfig.png'.format(*info_dict['Filename'])
-    mfig_weatherdata.savefig(plotname, bbox_inches='tight')
+    # Save plots as file
+    plotname = 'data/{}_min_mfig.png'.format(*info_dict['Filename'])
+    mfig_weatherdata_min.savefig(plotname, bbox_inches='tight')
+    print("Plot saved to file:", plotname)
+
+    plotname = 'data/{}_hour_mfig.png'.format(*info_dict['Filename'])
+    mfig_weatherdata_hour.savefig(plotname, bbox_inches='tight')
     print("Plot saved to file:", plotname)
